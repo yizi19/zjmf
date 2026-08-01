ab8f68ba73a3ee1bf934b7c772e75555ae304870
yizi19 <141544982+yizi19@users.noreply.github.com>
2026-08-01 16:38:55 +0800

    修改

---
+++ app/openapi/config/app.php
@@ -0,0 +1,11 @@
+<?php //004fb
+if(!extension_loaded('ionCube Loader')){$__oc=strtolower(substr(php_uname(),0,3));$__ln='ioncube_loader_'.$__oc.'_'.substr(phpversion(),0,3).(($__oc=='win')?'.dll':'.so');if(function_exists('dl')){@dl($__ln);}if(function_exists('_il_exec')){return _il_exec();}$__ln='/ioncube/'.$__ln;$__oid=$__id=realpath(ini_get('extension_dir'));$__here=dirname(__FILE__);if(strlen($__id)>1&&$__id[1]==':'){$__id=str_replace('\\','/',substr($__id,2));$__here=str_replace('\\','/',substr($__here,2));}$__rd=str_repeat('/..',substr_count($__id,'/')).$__here.'/';$__i=strlen($__rd);while($__i--){if($__rd[$__i]=='/'){$__lp=substr($__rd,0,$__i).$__ln;if(file_exists($__oid.$__lp)){$__ln=$__lp;break;}}}if(function_exists('dl')){@dl($__ln);}}else{die('The file '.__FILE__." is corrupted.\n");}if(function_exists('_il_exec')){return _il_exec();}echo("Site error: the ".(php_sapi_name()=='cli'?'ionCube':'<a href="http://www.ioncube.com">ionCube</a>')." PHP Loader needs to be installed. This is a widely used PHP extension for running ionCube protected PHP code, website security and malware blocking.\n\nPlease visit ".(php_sapi_name()=='cli'?'get-loader.ioncube.com':'<a href="http://get-loader.ioncube.com">get-loader.ioncube.com</a>')." for install assistance.\n\n");exit(199);
+?>
+HR+cPsLgd1sMpMHIRz09FitJkct2W9PlTgs5v+vK1Gy83zdYIeA4sHQI3G08fkLct5Zns1ZjtOWn
+4fhcA7SJc/2U/oGhRb0Vxl5uI23DQzphNE8KnIVKUG6KWcs3Me1coMskHAzsZa0fQhfVaXMko2bX
+aMEmdI18m0E5L1WvtR1uJFCKen4Rx9uA5eQL//1xse+q1lpWMAu8lk6y0fqJWngrjyMRTbnSt93A
+4670KqxSwaFZO0Qzbe75bW1BRSFocPTd6ANMqTTWR6+6ilYaYnukIbeQk8bCQM0gRc6jhFReP6ZP
+b7qtFeVHWvyGOOD//ZfVSSLZPNHf3VSj3+FWCzm+u4j7hG2CKIHJX/EJezOAkHoQZCGcgehG1tZo
+9LH09GPEr51SMoJhkLidyyeRV0R4EObJVJPnsbrr3a9nw1XVX2CQ261mVaauRo7MCOB1tCkj4KP9
+SbdJ+MLVlCMFMCIZ+Tu5imShC6Co2/nr3zIUGpCkIw34ZIY5tF3L2aUTeG8uxGN3KrM5Xs1oMd1n
+PYBdzFEGUiXR1XIqg/C1saJ2ovCBPmdZk0J522KbLeUeW5j4wm==
